eaecd28905806b4b0874999060483d34f92bdfe1
Nathan Hartzler <hartzlernathan@gmail.com>
2016-06-17 21:47:51 +0000

    initial python program

---
+++ sorting-hat.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+# sorting-hat.py
+# python 2.7.9
+# Author: Nathan Hartzler
+# License: MIT
+#
+# Sorting Hat python code to respond to keyboard input,
+#   playback House audio files and control Animatronic servos
+#
+
+import sys
+
+griff_key = '5~'
+huff_key = 'OH'
+raven_key = '6~'
+slith_key = 'OF'
+
+# helper
+def read_single_keypress():
+    """Waits for a single keypress on stdin.
+
+    This is a silly function to call if you need to do it a lot because it has
+    to store stdin's current setup, setup stdin for reading single keystrokes
+    then read the single keystroke then revert stdin back after reading the
+    keystroke.
+
+    Returns the character of the key that was pressed (zero on
+    KeyboardInterrupt which can happen when a signal gets handled)
+
+    """
+    import termios, fcntl, sys, os
+    fd = sys.stdin.fileno()
+    # save old state
+    flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
+    attrs_save = termios.tcgetattr(fd)
+    # make raw - the way to do this comes from the termios(3) man page.
+    attrs = list(attrs_save) # copy the stored version to update
+    # iflag
+    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK 
+                  | termios.ISTRIP | termios.INLCR | termios. IGNCR 
+                  | termios.ICRNL | termios.IXON )
+    # oflag
+    attrs[1] &= ~termios.OPOST
+    # cflag
+    attrs[2] &= ~(termios.CSIZE | termios. PARENB)
+    attrs[2] |= termios.CS8
+    # lflag
+    attrs[3] &= ~(termios.ECHONL | termios.ECHO | termios.ICANON
+                  | termios.ISIG | termios.IEXTEN)
+    termios.tcsetattr(fd, termios.TCSANOW, attrs)
+    # turn off non-blocking
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags_save & ~os.O_NONBLOCK)
+    # read a single keystroke
+    try:
+        ret = sys.stdin.read(1) # returns a single character
+    except KeyboardInterrupt: 
+        ret = 0
+    finally:
+        # restore old state
+        termios.tcsetattr(fd, termios.TCSAFLUSH, attrs_save)
+        fcntl.fcntl(fd, fcntl.F_SETFL, flags_save)
+    return ret
+
+
+
+def main():
+    button = ""
+    while button != "q":
+        button = read_single_keypress()
+        print "Got button", repr(button)
+        button = button[-2:]
+        if button == griff_key:
+            print "Griffindore!"
+        elif button == huff_key:
+            print "Hufflepuff!"
+        elif button == raven_key:
+            print "Ravenclaw!"
+        elif button == slith_key:
+            print "Slytherin!"
+        else:
+            print "NOT RECOGNIZED"
+
+
+main()
