06b553396dee43c9ebcef1429b9942e7f7e598b6
Nathan Hartzler <hartzlernathan@gmail.com>
2016-06-21 14:59:43 +0000

    Added audio and random selection code

--- sorting-hat.py
+++ sorting-hat.py
@@ -9,14 +9,23 @@
 #   playback House audio files and control Animatronic servos
 #
 
+import os
+import random
+import time
+import serial
 import sys
+import collections
 
-griff_key = '5~'
-huff_key = 'OH'
-raven_key = '6~'
-slith_key = 'OF'
+GRIFF_KEY = '5'
+HUFF_KEY = 'D'
+RAVEN_KEY = 'C'
+SLYTH_KEY = '6'
 
-# helper
+BASE_AUDIO_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "audio")
+
+WAIT_TIME = .5
+
+
 def read_single_keypress():
     """Waits for a single keypress on stdin.
 
@@ -62,24 +71,91 @@ def read_single_keypress():
         fcntl.fcntl(fd, fcntl.F_SETFL, flags_save)
     return ret
 
+def list_all_sound_files(subdir):
+    """
+    Get all the sound files from a subdir in the audio
+    folder
+    :param subdir:
+        Subdir to list
+    :return:
+        all sound files in subdir
+    """
+    return [f for f in os.listdir(
+        os.path.join(BASE_AUDIO_DIR, subdir)
+    ) if f[-4:] == '.wav']
 
+def get_full_path(subdir, audio_filename):
+    """
+    given an audio file and the subdir give a full path
+    from the base sound dir
+    :param audio_filename:
+        string, filename
+    :return:
+        full path from audio dir
+    """
+    return os.path.join(
+        BASE_AUDIO_DIR,
+        subdir,
+        audio_filename
+    )
+
+def get_random_wav_file(subdir):
+    """
+    Used to play a sound matching a category
+    :param subdir:
+        Which subdir to pull the file out of. Base dir is audio
+    :return:
+        A full path to the sound file chosen.
+    """
+    return get_full_path(
+        subdir,
+        random.choice(
+            list_all_sound_files(subdir)
+        )
+    )
+
+def play_script(house):
+    # Consider some stalling lines
+    script = list_all_sound_files('stalling')
+    random.shuffle(script)
+    script = [get_full_path('stalling', f)
+              for f in script
+              if random.random() < .1]
+
+    # Consider shouting I know!
+    if random.random() < .5:
+        script.append(get_random_wav_file('know'))
+
+    # Append the house
+    script.append(get_full_path('houses', house + '.wav'))
+
+    for sound in script:
+        os.system('aplay ' + sound)
+        time.sleep(WAIT_TIME)  
 
 def main():
+    print "Sorting Hat v1.0"
+    print "Press 'q' to quit"
+    print "Waiting for remote input..."
     button = ""
     while button != "q":
+        selected_house = ''
         button = read_single_keypress()
-        print "Got button", repr(button)
-        button = button[-2:]
-        if button == griff_key:
-            print "Griffindore!"
-        elif button == huff_key:
+        if button == GRIFF_KEY:
+            print "Gryffindor!"
+            selected_house = "gryffindor"
+        elif button == HUFF_KEY:
             print "Hufflepuff!"
-        elif button == raven_key:
+            selected_house = "hufflepuff"
+        elif button == RAVEN_KEY:
             print "Ravenclaw!"
-        elif button == slith_key:
+            selected_house = "ravenclaw"
+        elif button == SLYTH_KEY:
             print "Slytherin!"
+            selected_house = "slytherin"
         else:
-            print "NOT RECOGNIZED"
-
-
+            continue
+        if selected_house != '':
+            play_script(selected_house)
+            
 main()
